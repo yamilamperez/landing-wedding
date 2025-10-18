c9097e8d61e99e5b4b3b18f941db3f25deee0bd3
Yamila_Perez <yamilaperez.dev@gmail.com>
2025-10-18 00:59:42 -0300

    Cambio en ruta de imagen

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import '../styles/components/Playlist.css'
+import playlistImage from '../assets/images/Imagen-cassette-personalizada.png'
+
 
 function Playlist() {
 return (
     <section className="playlist">
         <div>
             <a href="https://open.spotify.com/playlist/7kA1Y0jxj78fPubXr55Jap?si=ebc924cf6a3e4f86">
-                <img src="../src/assets/images/Imagen-cassette-personalizada.png" alt="Cassete de música con nuestra propia playlist para reproducir en Spotify" />
+                <img src={playlistImage} alt="Cassete de música con nuestra propia playlist para reproducir en Spotify" />
             </a>       
         </div>
     </section>
