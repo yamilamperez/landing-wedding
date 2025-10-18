a0db16135bbdecc62930a10bd03afa9a4630c73f
Yamila_Perez <yamilaperez.dev@gmail.com>
2025-10-18 00:51:45 -0300

    Corrección imagen

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -6,7 +6,7 @@ return (
     <section className="playlist">
         <div>
             <a href="https://open.spotify.com/playlist/7kA1Y0jxj78fPubXr55Jap?si=ebc924cf6a3e4f86">
-                <img src="./src/assets/images/Imagen-cassette-personalizada.png" alt="Cassete de música con nuestra propia playlist para reproducir en Spotify" />
+                <img src="../src/assets/images/Imagen-cassette-personalizada.png" alt="Cassete de música con nuestra propia playlist para reproducir en Spotify" />
             </a>       
         </div>
     </section>
