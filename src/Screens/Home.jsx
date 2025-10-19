0020f8164deeee17c5a04fb57d493e8ec1e02959
Yamila_Perez <yamilaperez.dev@gmail.com>
2025-10-18 23:19:32 -0300

    cambio error vercel

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from '../Components/Header'
+import Header from '../Components/Header';
 import Itinerary from '../Components/Itinerary';
 import RSVPForm from '../Components/RSVPForm';
 import Footer from '../Components/Footer';
