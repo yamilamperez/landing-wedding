fe79472f750e5d6d8c63c3bce962d3fa8a36d06d
Yamila_Perez <yamilaperez.dev@gmail.com>
2025-10-20 20:04:25 -0300

    cc

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Header from '../Components/Header';
 import Itinerary from '../Components/Itinerary';
 import RSVPForm from '../Components/RSVPForm';
 import Footer from '../Components/Footer';
 import '../styles/global.css';
 import Gift from '../Components/Gift';
 import Playlist from '../Components/Playlist';
+import Header from '../Components/Header';
 
 
 function Home(){
     return(
     <div className="app-container">
-        <Header />
+        <Header/>
         <div className='row-container'>
             <div className='row-item-1'><Playlist /></div>
             <div className='row-item-2'><Itinerary /></div>
